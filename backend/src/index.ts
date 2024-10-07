169fcd2f8e547895056edc497cafe9d4d5a754a0
rahulchauhan04 <rahulg0004@gmail.com>
2024-10-08 01:37:26 +0530

    Setup typescript backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,16 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import cors from 'cors';
-import "dotenv/config";
+import 'dotenv/config';
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(cors())
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get("/api/test", (req: Request, res: Response) => {
+    res.json({ message: "Hello from express endpoint!" });
+});
+
+app.listen(8000, () => {
+    console.log("Server running on localhost:8000");
+});

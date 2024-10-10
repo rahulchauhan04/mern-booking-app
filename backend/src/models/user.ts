9ee7b9d872003966d3a2c5b9cda76d3d9f7095af
rahulchauhan04 <rahulg0004@gmail.com>
2024-10-10 23:11:53 +0530

    user model

--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -9,5 +9,12 @@ export type UserType = {
 }
 
 const userSchema = new mongoose.Schema({
-     
+     email: { type: String, required: true, unique: true },
+     password: { type: String, required: true },
+     firstName: { type: String, required: true },
+     lastName: { type: String, required: true },
 })
+
+const User = mongoose.model<UserType>("User", userSchema);
+
+export default User;

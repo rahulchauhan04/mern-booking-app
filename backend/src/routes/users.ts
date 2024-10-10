f4a5cc09db9e9779fed6415a08cbb2acc5ba530a
rahulchauhan04 <rahulg0004@gmail.com>
2024-10-10 23:22:47 +0530

    user routes

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,10 +1,19 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
+import User from '../models/user';
 
 const router = express.Router()
 
 router.post("/register", async(req: Request, res: Response) => {
      try {
-               
+          let user = await User.findOne({
+               email: req.body.email,
+          })
+
+          if(user){
+               return res.status(400).json({
+                    message: "User already exists",
+               })
+          }
      } catch (error) {
           
      }

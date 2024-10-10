2797590d5a12a8432c2012988e561e2527dd18d0
rahulchauhan04 <rahulg0004@gmail.com>
2024-10-10 23:18:24 +0530

    routes

---
+++ backend/src/routes/users.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const router = express.Router()
+
+router.post("/register", async(req: Request, res: Response) => {
+     try {
+               
+     } catch (error) {
+          
+     }
+})

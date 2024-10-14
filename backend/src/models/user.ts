37eebd5d818843be01099f9120ae635a5effa198
rahulchauhan04 <rahulg0004@gmail.com>
2024-10-14 23:04:35 +0530

    user Routes

--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
+// Define the UserType type
 export type UserType = {
      _id: string;
      email: string;
@@ -8,13 +10,23 @@ export type UserType = {
      lastName: string;
 }
 
+// Create a new mongoose schema for the User model
 const userSchema = new mongoose.Schema({
-     email: { type: String, required: true, unique: true },
-     password: { type: String, required: true },
+     email: { type: String, required: true, unique: true }, 
+     password: { type: String, required: true }, 
      firstName: { type: String, required: true },
-     lastName: { type: String, required: true },
+     lastName: { type: String, required: true }, 
 })
+
+// Pre-save hook to hash the password before saving the user document
+userSchema.pre("save", async function(next){
+     if(this.isModified("password")){ 
+          this.password = await bcrypt.hash(this.password, 10); // Hash the password with a salt round of 10
+     }
+     next(); 
+})
+
 
 const User = mongoose.model<UserType>("User", userSchema);
 
-export default User;
+export default User; 
--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,6 +1,6 @@
 import express, {Request, Response} from 'express';
 import User from '../models/user';
-
+import jwt from 'jsonwebtoken';
 const router = express.Router()
 
 router.post("/register", async(req: Request, res: Response) => {
@@ -14,7 +14,25 @@ router.post("/register", async(req: Request, res: Response) => {
                     message: "User already exists",
                })
           }
+          user = new User(req.body)
+          await user.save()
+
+          const token = jwt.sign({userId: user._id}, 
+               process.env.JWT_SECRET_KEY as string,
+               {expiresIn: "90d"}
+          );
+
+          res.cookie("auth_token", token, {
+               httpOnly: true,
+               secure: process.env.NODE_ENV === "production",
+               maxAge: 90 * 24 * 60 * 60 * 1000,
+          })
+          return res.sendStatus(200);
+
      } catch (error) {
-          
+          console.log(error)
+          res.status(500).json({ message: "Something went wrong" });
      }
 })
+
+export default router;

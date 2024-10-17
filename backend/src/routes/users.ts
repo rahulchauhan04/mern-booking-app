f44072875842ce7f9ffe6d9f9131ebdc7489a301
rahulchauhan04 <rahulg0004@gmail.com>
2024-10-17 21:33:37 +0530

    wrapper function typescript inference

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,38 +1,42 @@
-import express, {Request, Response} from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import User from '../models/user';
 import jwt from 'jsonwebtoken';
-const router = express.Router()
 
-router.post("/register", async(req: Request, res: Response) => {
-     try {
-          let user = await User.findOne({
-               email: req.body.email,
-          })
 
-          if(user){
-               return res.status(400).json({
-                    message: "User already exists",
-               })
-          }
-          user = new User(req.body)
-          await user.save()
+const router = express.Router();
 
-          const token = jwt.sign({userId: user._id}, 
-               process.env.JWT_SECRET_KEY as string,
-               {expiresIn: "90d"}
-          );
+// Wrapper function to handle async errors
+const wrapAsync = (fn: Function) => (req: Request, res: Response, next: NextFunction) => {
+     Promise.resolve(fn(req, res, next)).catch(next);
+};
 
-          res.cookie("auth_token", token, {
-               httpOnly: true,
-               secure: process.env.NODE_ENV === "production",
-               maxAge: 90 * 24 * 60 * 60 * 1000,
-          })
-          return res.sendStatus(200);
 
-     } catch (error) {
-          console.log(error)
-          res.status(500).json({ message: "Something went wrong" });
-     }
-})
+//api/users/register
+router.post("/register", wrapAsync(async (req: Request, res: Response) => {
+  let user = await User.findOne({
+    email: req.body.email,
+  });
+
+  if (user) {
+    return res.status(400).json({
+      message: "User already exists",
+    });
+  }
+
+  user = new User(req.body);
+  await user.save();
+
+  const token = jwt.sign({ userId: user._id },
+    process.env.JWT_SECRET_KEY as string,
+    { expiresIn: "90d" }
+  );
+
+  res.cookie("auth_token", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    maxAge: 90 * 24 * 60 * 60 * 1000,
+  });
+  return res.sendStatus(200);
+}));
 
 export default router;

88b63d68dab49858982f888486f54a6adb3ab9fe
rahulchauhan04 <rahulg0004@gmail.com>
2024-10-21 23:52:24 +0530

    Auth

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,5 +1,20 @@
-import express from "express";
+import express, {Request, Response} from "express";
+import { check, validationResult } from "express-validator";
+import wrapAsync from "../middleware/wrapAsync";
 
 const router = express.Router();
 
-router.post("/login", [])
+router.post("/login", [
+     check("email", "Email is required").isEmail(),
+     check("password", "Password with 6 or more character required").isLength({min:6}),
+     ], wrapAsync(async (req: Request, res: Response) => {
+     // Login logic here
+
+     const errors = validationResult(req);
+
+     if(!errors.isEmpty()){
+          return res.status(400).json({ message: errors.array()});
+     }
+}))
+
+export default router;

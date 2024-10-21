83e141097ab9038614f4a1da81e850664e2ce031
rahulchauhan04 <rahulg0004@gmail.com>
2024-10-21 23:59:11 +0530

    auth

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,6 +1,8 @@
 import express, {Request, Response} from "express";
 import { check, validationResult } from "express-validator";
 import wrapAsync from "../middleware/wrapAsync";
+import User from "../models/user";
+import bcrypt from "bcryptjs";
 
 const router = express.Router();
 
@@ -14,6 +16,18 @@ router.post("/login", [
 
      if(!errors.isEmpty()){
           return res.status(400).json({ message: errors.array()});
+     }
+
+     const { email, password } = req.body;
+
+     try {
+          const user = await User.findOne({ email });
+          if (!user) {
+               return res.status(400).json({ message: "Invalid credentials" });
+          }
+          const isMatch = await bcrypt.compare(password, user.password);
+     } catch (error) {
+          res.status(500).json({ message: "Something went wrong"})
      }
 }))
 

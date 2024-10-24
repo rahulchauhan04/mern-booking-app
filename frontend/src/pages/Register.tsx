aaeeb477cfdd95049679233354d9931a76177627
rahulchauhan04 <rahulg0004@gmail.com>
2024-10-24 22:18:57 +0530

    register page form

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,6 +1,6 @@
 import { useForm } from "react-hook-form";
 
-type RegisterFormData = {
+export type RegisterFormData = {
      firstName: string;
      lastName: string;
      email: string;
@@ -10,23 +10,35 @@ type RegisterFormData = {
 
 const Register = () => {
 
-     const { register, watch } = useForm<RegisterFormData>();
+     const { register, watch, handleSubmit, formState: {errors} } = useForm<RegisterFormData>();
+
+     const onSubmit = handleSubmit((data) => {
+          console.log(data);
+     })
 
      return (
-          <form className="flex flex-col gap-5">
+          <form className="flex flex-col gap-5" onSubmit={onSubmit}>
                <h2 className="text-3xl font-bold">Create an Account</h2>
                <div className="flex flex-col md:flex-row gap-5">
                     <label className="text-gray-700 text-sm font-bold flex-1">
                          First Name
                          <input
                               className="border w-full rounded py-1 px-2 font-normal"
-                              {...register("firstName", { required: "This field is required" })} />
+                              {...register("firstName", { required: "This field is required" })}> 
+                         </input>
+                         {errors.firstName && (
+                              <span className="text-red-500">{errors.firstName.message}</span>
+                         )}
                     </label>
                     <label className="text-gray-700 text-sm font-bold flex-1">
                          Last Name
                          <input
                               className="border w-full rounded py-1 px-2 font-normal"
-                              {...register("lastName", { required: "This field is required" })} />
+                              {...register("lastName", { required: "This field is required" })}>
+                         </input>
+                         {errors.lastName && (
+                              <span className="text-red-500">{errors.lastName.message}</span>
+                         )}
                     </label>
                </div>
                <label className="text-gray-700 text-sm font-bold flex-1">
@@ -34,7 +46,11 @@ const Register = () => {
                     <input
                          type="email"
                          className="border w-full rounded py-1 px-2 font-normal"
-                         {...register("email", { required: "This field is required" })} />
+                         {...register("email", { required: "This field is required" })}>
+                    </input>
+                    {errors.email && (
+                         <span className="text-red-500">{errors.email.message}</span>
+                    )}
                </label>
                <label className="text-gray-700 text-sm font-bold flex-1">
                     Password
@@ -47,7 +63,11 @@ const Register = () => {
                                    value: 6,
                                    message: "Password must be at least 6 characters"
                               },
-                         })} />
+                         })}>
+                    </input>
+                    {errors.password && (
+                         <span className="text-red-500">{errors.password.message}</span>
+                    )}
                </label>
                <label className="text-gray-700 text-sm font-bold flex-1">
                     Confirm Password
@@ -62,8 +82,19 @@ const Register = () => {
                                         return "Your Passwords do not match";
                                    }
                               }
-                         })} />
+                         })}>
+                    </input>
+                    {errors.confirmPassword && (
+                         <span className="text-red-500">{errors.confirmPassword.message}</span>
+                    )}
                </label>
+               <span>
+                    <button 
+                         type="submit"
+                         className="bg-blue-600 text-white p-2 font-bold hover:bg-blue-500 text-xl">
+                         Create Account
+                    </button>
+               </span>
           </form>
      );
 };
